269a484cbd2bfa74790084d73caf03f0764db424
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-10-09 21:30:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // 👈 this makes CSS and JS use relative paths
+  base: '/', // ✅ use root path for Vercel
   plugins: [tailwindcss(), react()],
   build: {
     outDir: 'dist',
